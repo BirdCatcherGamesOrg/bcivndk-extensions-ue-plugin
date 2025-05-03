d5087ed3cd8cb980ff8aded3e0307a37883eae8f
BirdCatcherGames <birdcatchergames@gmail.com>
2025-05-03 13:59:10 -0700

    Adding a widget for showing the main, center dialogue UI
    Composable with other UUserWidget's and slots to make the entire UI

---
+++ Source/BCIVNDKDialogue/Public/Widget/MainNarrationWidget.h
@@ -0,0 +1,28 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainNarrationWidget.generated.h"
+
+/**
+ * @brief UBCIVNDKDialogueMainNarrationWidget holds the main narration dialogue box and controls.
+ */
+UCLASS(MinimalAPI)
+class UBCIVNDKDialogueMainNarrationWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditDefaultsOnly, Transient, meta=(BindWidget))
+	TObjectPtr<class UNamedSlot> DialogueUISlot;
+	
+	UPROPERTY(EditDefaultsOnly, Transient, meta=(BindWidget))
+	TObjectPtr<class UNamedSlot> ShowHideDialogueUISlot;
+	
+	UPROPERTY(EditDefaultsOnly, Transient, meta=(BindWidget))
+	TObjectPtr<class UNamedSlot> DialogueHistorySlot;
+	
+};
