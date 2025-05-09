bebdfcef8530c27b8115e771e42437055c4ae906
BirdCatcherGames <birdcatchergames@gmail.com>
2025-05-09 12:53:26 -0700

    Adding the UBCIVNDKCharacterPoseAfterImage to the Character module
    This component provides cross-fades for character's poses, gracefully transitioning without the need to fully animate every character movement
    
    Example usage:
    
    ```
    ABCIVNDKPlayerCharacter::ABCIVNDKPlayerCharacter(const FObjectInitializer& ObjectInitializer)
    {
        // Other initialization logic above...
    
        PoseAfterImage = CreateDefaultSubobject<UBCIVNDKCharacterPoseAfterImage>(TEXT("PoseAfterImage"));
    }
    ```
    
    In PostInitializeComponents
    
    ```
    void ABCIVNDKPlayerCharacter::PostInitializeComponents()
    {
    	Super::PostInitializeComponents();
    	// Other PostInitializeComponents logic.
    	// Suggested to do this here because the Poseable meshes have transient data in them
    	// PostInitializeComponents is the earliest you can set this up, so this is guaranteed to work
    	// during BeginPlay
    	PoseAfterImage->SetDependencies(*GetMesh());
    }
    ```

---
+++ Source/BCIVNDKCharacter/Private/PoseAfterImage.cpp
@@ -0,0 +1,111 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+#include "PoseAfterImage.h"
+
+#include "Components/PoseableMeshComponent.h"
+
+UBCIVNDKCharacterPoseAfterImage::UBCIVNDKCharacterPoseAfterImage()
+{
+	CurrentPoseMesh = CreateDefaultSubobject<UPoseableMeshComponent>(TEXT("CurrentPoseMesh"));
+	NextPoseMesh = CreateDefaultSubobject<UPoseableMeshComponent>(TEXT("NextPoseMesh"));
+	bWantsInitializeComponent = true;
+	CurrentPoseMesh->SetVisibility(false);
+	NextPoseMesh->SetVisibility(false);
+}
+
+bool UBCIVNDKCharacterPoseAfterImage::CrossFadePoses()
+{
+	TArray<FName> BoneNames;
+	CurrentPoseMesh->GetBoneNames(BoneNames);
+	for (const FName BoneName : BoneNames)
+	{
+		FTransform Transform = BaseMesh->GetBoneTransform(BoneName, RTS_World);
+		CurrentPoseMesh->SetBoneTransformByName(BoneName, Transform, EBoneSpaces::Type::WorldSpace);
+	}
+	
+	for (UMaterialInstanceDynamic* MaterialInstance : CurrentMaterialInstances)
+	{
+		MaterialInstance->SetScalarParameterValue(DissolveParameterName, 1.0);
+	}
+
+	for (UMaterialInstanceDynamic* MaterialInstance : NextPoseMaterialInstances)
+	{
+		MaterialInstance->SetScalarParameterValue(DissolveParameterName, 0.0);
+	}
+	
+	DissolveTime = 0.0f;
+	
+	FTickerDelegate TickDelegate = FTickerDelegate::CreateWeakLambda(this, [this](float Delta)
+	{
+		if (!NextPoseMesh->IsVisible())
+		{
+			NextPoseMesh->SetVisibility(true);
+			TArray<FName> BoneNames;
+			NextPoseMesh->GetBoneNames(BoneNames);
+			for (const FName BoneName : BoneNames)
+			{
+				FTransform Transform = BaseMesh->GetBoneTransform(BoneName, RTS_World);
+				NextPoseMesh->SetBoneTransformByName(BoneName, Transform, EBoneSpaces::Type::WorldSpace);
+			}
+		}
+
+		float DissolveAmount = FMath::InterpEaseIn(1.0, 0.0, DissolveTime / DissolveOverSeconds, 2);
+		
+
+		for (UMaterialInstanceDynamic* MaterialInstance : CurrentMaterialInstances)
+		{
+			MaterialInstance->SetScalarParameterValue(DissolveParameterName, DissolveAmount);
+		}
+
+		float ForwardAmount = FMath::InterpEaseOut(0.0, 1.0, DissolveTime / DissolveOverSeconds, 4);
+
+		for (UMaterialInstanceDynamic* MaterialInstance : NextPoseMaterialInstances)
+		{
+			MaterialInstance->SetScalarParameterValue(DissolveParameterName, ForwardAmount);
+		}
+
+		DissolveTime += Delta;
+		if (DissolveTime > DissolveOverSeconds || DissolveAmount <= 0.0)
+		{
+			CurrentPoseMesh->SetVisibility(false);
+			NextPoseMesh->SetVisibility(false);
+			BaseMesh->SetVisibility(true);
+			return false;
+		}
+		return true;
+	});
+
+	BaseMesh->SetVisibility(false);
+	CurrentPoseMesh->SetVisibility(true);
+	
+	DelegateHandle = FTSTicker::GetCoreTicker().AddTicker(TickDelegate);
+	
+	return true;
+}
+
+void UBCIVNDKCharacterPoseAfterImage::OnRegister()
+{
+	Super::OnRegister();
+	CurrentPoseMesh->RegisterComponent();
+	NextPoseMesh->RegisterComponent();
+	// Do not attach the poseable meshes, otherwise they'll snap to the movement of the character's mesh automatically.
+}
+
+void UBCIVNDKCharacterPoseAfterImage::InitializeComponent()
+{
+	Super::InitializeComponent();
+	for (int32 i = 0; i < CurrentPoseMesh->GetNumMaterials(); ++i)
+	{
+		CurrentMaterialInstances.Add(CurrentPoseMesh->CreateAndSetMaterialInstanceDynamic(i));
+	}
+	
+	for (int32 i = 0; i < NextPoseMesh->GetNumMaterials(); ++i)
+	{
+		NextPoseMaterialInstances.Add(NextPoseMesh->CreateAndSetMaterialInstanceDynamic(i));
+	}
+}
+
+void UBCIVNDKCharacterPoseAfterImage::SetDependencies(USkeletalMeshComponent& InSkeletalMeshComponent)
+{
+	BaseMesh = &InSkeletalMeshComponent;
+}
---
+++ Source/BCIVNDKCharacter/Public/PoseAfterImage.h
@@ -0,0 +1,89 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "PoseAfterImage.generated.h"
+
+/**
+ * @brief UBCIVNDKCharacterPoseAfterImage wraps poseable skeletal meshes and controls fading between them.
+ *
+ * Use this class to gracefully change a character's pose as a transition effect in the movie scene. This skips needing
+ * to fully animate a character's movements in the scene without resorting to camera changes. Custom dissolve materials
+ * can be provided, which can, for example, have different characters subtle fade with different thematic colors.
+ * e.g. antagonist cross-fades with a hint of red.
+ * 
+ * The approach is to swap the character's mesh for before and after poseable meshes,
+ * dissolve the before mesh, fade in the after, then swap the character's mesh back in.
+ * This creates a cross-fade for subtle poses like a head twisting or hand gesture,
+ * or big changes like moving to a new place.
+ *
+ * On the same frame the character's skeletal mesh moves:
+ * 1. Take a "snapshot" of the current bone transforms of the character's skeletal mesh BEFORE it moves.
+ * 2. Set the character's skeletal mesh to be invisible.
+ * 3. Set the CurrentPoseMesh to be visible.
+ * 4. Set the opacity of the CurrentPoseMesh's materials to be fully visible.
+ * 5. Wait a frame so the character's mesh moves to the new position
+ * 6. Set the NextPoseMesh bone transforms to the now updated position of the invisible character's mesh
+ * 7. Set the NextPoseMesh to be visible
+ * 8. Set the opacity of the NextPoseMesh to be completely invisible
+ * 9. Apply an easing function to the opacity of the Current and Next meshes to cross-fade between them
+ * 10. When the cross-fade completes, set the Poseable meshes to be invisible, make the character's mesh visible again.
+ */
+UCLASS(ClassGroup=(Custom), MinimalAPI, meta=(BlueprintSpawnableComponent))
+class UBCIVNDKCharacterPoseAfterImage : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:
+
+	UBCIVNDKCharacterPoseAfterImage();
+	
+	virtual void OnRegister() override;
+
+	virtual void InitializeComponent() override;
+	
+	BCIVNDKCHARACTER_API void SetDependencies(class USkeletalMeshComponent& InSkeletalMeshComponent);
+
+	BCIVNDKCHARACTER_API bool CrossFadePoses();
+
+private:
+	
+	UPROPERTY(EditAnywhere, meta=(AllowPrivateAccess=true))
+	TObjectPtr<class UPoseableMeshComponent> CurrentPoseMesh;
+
+	UPROPERTY(EditAnywhere, meta=(AllowPrivateAccess=true))
+	TObjectPtr<class UPoseableMeshComponent> NextPoseMesh;
+	
+	UPROPERTY(VisibleAnywhere, Transient, meta=(AllowPrivateAccess=true))
+	TObjectPtr<class USkeletalMeshComponent> BaseMesh;
+
+	/**
+	 * TODO: https://github.com/BirdCatcherGamesOrg/bcivndk-ue-plugin/issues/1
+	 */
+	
+	UPROPERTY(EditAnywhere, meta=(AllowPrivateAccess=true))
+	FName DissolveParameterName;
+
+	/**
+	 * TODO: https://github.com/BirdCatcherGamesOrg/bcivndk-ue-plugin/issues/1
+	 */
+	
+	UPROPERTY(EditAnywhere, meta=(AllowPrivateAccess=true))
+	float DissolveOverSeconds = 0.0f;
+
+	UPROPERTY(VisibleAnywhere, meta=(AllowPrivateAccess=true))
+	float DissolveTime = 0.0f;
+	
+	UPROPERTY(EditAnywhere, Transient, meta=(AllowPrivateAccess=true))
+	TArray<class UMaterialInstanceDynamic*> CurrentMaterialInstances;
+
+
+	UPROPERTY(EditAnywhere, Transient, meta=(AllowPrivateAccess=true))
+	TArray<class UMaterialInstanceDynamic*> NextPoseMaterialInstances;
+	
+	FTSTicker::FDelegateHandle DelegateHandle;
+	
+	
+};
